3266060b353f6b4404e27f91dfe2a60cb115ce4b
ShuqiYang <49054767+shuqiyang@users.noreply.github.com>
2020-04-22 14:26:05 +0800

    Add files via upload

---
+++ stopvms.ps1
@@ -0,0 +1,32 @@
+﻿### START - Stopping Azure VMs ####
+$VMFile = "D:\your dir\AzureVMs.txt"
+$ReportFile = "D:\your dir\VMStopReport.txt"
+
+Connect-AzAccount -Environment AzureChinaCloud -Subscription 'your subscription ID'
+
+Add-Content $ReportFile "---------------------------------------------"
+$timestamp = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
+$vmcount=1
+Add-Content $ReportFile $timestamp
+Foreach ($ThisVMNow in Get-Content "$VMFile")
+{
+
+    $Error.Clear()
+    $RGnow = ($ThisVMNow -split ' ')[0]
+    $VMnow = ($ThisVMNow -split ' ')[1]
+    Stop-AzVM -ResourceGroupName $RGnow -Name $VMnow -Force
+    IF ($Error.Count -eq 0)
+    {
+        $STR = "("+$vmcount + ")Azure Virtual Machine was stopped : " + $ThisVMNow
+        Add-Content $ReportFile $STR
+    }
+    else
+    {
+        $STR = "("+$vmcount + ")ERROR: Failed to stop Azure Virtual Machine : " + $ThisVMNow+","+$Error[0]
+        Add-Content $ReportFile $STR
+    }
+    $vmcount++
+}
+$STR = "End of operation log report @" + $timestamp
+Add-Content $ReportFile $STR
+### END - Stopping Azure VMs ####
